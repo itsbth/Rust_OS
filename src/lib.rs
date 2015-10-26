513eeb8600b3d77451f5d81dbca7525c4ce6e17b
Bjørn Tore Håvie <itsbth@itsbth.com>
2015-10-26 22:55:57 +0100

    Works, mostly.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,27 @@
 #![feature(no_std, lang_items)]
+#![feature(const_fn, unique, core_str_ext, asm)]
 #![no_std]
+
 extern crate rlibc;
+extern crate spin;
+
+#[macro_use]
+mod vga_buffer;
+
+unsafe fn inb(port: u16) -> u8 {
+    let mut ret: u8;
+    asm!("inb $1,$0" : "={ax}" (ret) : "{dx}" (port) :: "volatile");
+    ret
+}
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let hello = b"Hello, World!";
-    let color_byte = 0x1f;
-
-    let mut hello_colored = [color_byte; 24];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
-
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
-    // loop{}
+    use core::fmt::Write;
+    vga_buffer::clear_screen();
+    vga_buffer::WRITER.lock().write_str("Hello, World!\n");
+    println!("Some numbers: {} {}", 42, 13.37);
+    println!("KBSTATP: {}", unsafe {inb(0x64)});
+    loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
